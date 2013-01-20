05b638821139920aeece3ca0709f3db26befe624
Tony Pisculli <tony@hawaiishakes.org>
2013-01-19 20:55:03 -1000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = '4d66d1d5c98c2207b449eea5c097482ceb111664b8cf7f9ba6a89ed9b2e592f69b3f6110bc5ec9ebe4e2310f2c597be4692a035c379a22b27e6b7a9b70b83e5d'
